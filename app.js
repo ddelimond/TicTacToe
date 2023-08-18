6b6904f76ae5e04faf9e0abc01c43376aea39e6b
Darren Delimond <darrendelimond@gmail.com>
2023-08-17 23:40:11 -0400

    =now able to mark the users choice and determine a winner of the round

--- app.js
+++ app.js
@@ -7,21 +7,24 @@ gameBoard = (function(){
         boardContainer = document.querySelector(".gameBoard");
 
          boardContainer.innerHTML = boardSpots.map((space, index) =>{
-                return `<button class="gameSpot" id="index-${index}">${space}</button>`
+                return `<div class="gameSpot" id="index-${index}">${space}</div>`
         }).join("");
     }
 
-
     function markPlayerMove(spotIndex, playerMarker){
       boardSpots[spotIndex] = playerMarker;
-      console.log(boardSpots);
-    }
+      let mark = document.createTextNode(`${playerMarker}`);
+      document.getElementById(`index-${spotIndex}`).append(mark);
+}
 
-
+function getMarkedSpotsArr(){
+      return boardSpots;
+}
 
     return{
         render,
-        markPlayerMove
+        markPlayerMove,
+        getMarkedSpotsArr
     }
 })()
 
@@ -113,16 +116,52 @@ function getPlayer2Score(){
 function updateGameBoard(e){
      whosTurnIsIt();
      let selectedSpotIndex = parseInt(e.target.id.split("-")[1]);
-    let activePlayerMarker = activePlayer.marker;
-    gameBoard.markPlayerMove(selectedSpotIndex,activePlayerMarker);
-    gameBoard.render();
-    setGameSpotsEventListeners();
+     let activePlayerMarker = activePlayer.marker;
+     let gameBoardSpot = e.target;
+     gameBoard.markPlayerMove(selectedSpotIndex,activePlayerMarker);
+     gameBoardSpot.classList.add("marked");
+     gameBoardSpot.removeEventListener('click',updateGameBoard);
     endTurn();
+    determineWinner(activePlayerMarker);
     }
 
 
+    function winningConditionalStatement(index1,index2,index3, marker){
+        let markedSpotsArr = gameBoard.getMarkedSpotsArr();
+        return gameBoard.getMarkedSpotsArr()[index1] === marker && gameBoard.getMarkedSpotsArr()[index2] === marker && gameBoard.getMarkedSpotsArr()[index3] === marker;
+    }
 
+    function getWinnersName(marker){
+     return player1.marker === marker? console.log(`Winner is ${player1.name}`):console.log(`Winner is ${player2.name}`);
+    }
 
+    function determineWinner(playerMarker){
+    console.log(playerMarker)
+     if(winningConditionalStatement(0,1,2, playerMarker)){
+     return getWinnersName(playerMarker);
+    }
+    else if(winningConditionalStatement(3,4,5, playerMarker)){
+        return getWinnersName(playerMarker);
+     }
+     else if(winningConditionalStatement(6,7,8, playerMarker)){
+         return getWinnersName(playerMarker);
+     }
+     else if(winningConditionalStatement(0,3,6, playerMarker)){
+         return getWinnersName(playerMarker);
+     }
+     else if(winningConditionalStatement(1,4,7, playerMarker)){
+         return getWinnersName(playerMarker);
+     }
+     else if(winningConditionalStatement(2,5,8, playerMarker)){
+         return getWinnersName(playerMarker);
+     }
+     else if(winningConditionalStatement(0,4,8, playerMarker)){
+         return getWinnersName(playerMarker);
+     }
+     else if(winningConditionalStatement(2,4,6, playerMarker)){
+         return getWinnersName(playerMarker);
+     }
+}
 
 
     return{
@@ -143,3 +182,13 @@ function Player(name, marker){
 }
 
 document.querySelector(".startBtn").addEventListener("click", gameController.start)
+
+
+
+
+
+
+
+
+
+
